2e11c400770d78082be277fb271c453c8d606881
Tran Hoang Duy <duytrieudong@gmail.com>
2023-02-10 22:56:34 +0700

    code chức năng tìm kiếm sản phẩm

--- ShopeeClone/src/pages/ProductList/ProductList.tsx
+++ ShopeeClone/src/pages/ProductList/ProductList.tsx
@@ -1,34 +1,17 @@
 import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import useQueryParams from 'src/hooks/useQueryParams'
 import AsideFilter from './components/AsideFilter'
 import Product from './components/Product/Product'
 import SortProductList from './components/SortProductList'
 import { productApi } from 'src/apis/product.api'
 import Pagination from 'src/components/Pagination'
 import { ProductListConfig } from 'src/types/product.type'
-import { omitBy, isUndefined } from 'lodash'
+
 import categoryApi from 'src/apis/category.api'
-export type QueryConfig = {
-  [key in keyof ProductListConfig]: string
-}
+import useQueryConfig from 'src/hooks/useQueryConfig'
+
 const ProductList = () => {
-  const queryParams: QueryConfig = useQueryParams()
-  const queryConfig: QueryConfig = omitBy(
-    {
-      page: queryParams.page || '1',
-      limit: queryParams.limit,
-      exclude: queryParams.exclude,
-      order: queryParams.order,
-      name: queryParams.name,
-      price_min: queryParams.price_min,
-      price_max: queryParams.price_max,
-      rating_filter: queryParams.rating_filter,
-      sort_by: queryParams.sort_by,
-      category: queryParams.category
-    },
-    isUndefined
-  )
+  const queryConfig = useQueryConfig()
 
   const { data: productsData } = useQuery({
     queryKey: ['products', queryConfig],
