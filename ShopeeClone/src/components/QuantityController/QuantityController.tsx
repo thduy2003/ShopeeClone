cee0b451d6e1752319a70d4e09a523327515e46b
Tran Hoang Duy <duytrieudong@gmail.com>
2023-02-13 14:18:22 +0700

    cập nhật inputNumber và quantityController với local state component

--- ShopeeClone/src/components/QuantityController/QuantityController.tsx
+++ ShopeeClone/src/components/QuantityController/QuantityController.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputNumber, { InputNumberProps } from '../InputNumber'
 interface Props extends InputNumberProps {
   max?: number
@@ -17,6 +17,7 @@ const QuantityController = ({
   classNameWrapper = 'ml-10',
   ...rest
 }: Props) => {
+  const [localValue, setLocalValue] = useState<number>(Number(value || 0))
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(event.target.value)
     if (max !== undefined && _value > max) {
@@ -25,20 +26,23 @@ const QuantityController = ({
       _value = 1
     }
     onType && onType(_value)
+    setLocalValue(_value)
   }
   const increase = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
     }
     onIncrease && onIncrease(_value)
+    setLocalValue(_value)
   }
   const decrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
     onDecrease && onDecrease(_value)
+    setLocalValue(_value)
   }
   return (
     <div className={' flex items-center' + classNameWrapper}>
@@ -58,7 +62,7 @@ const QuantityController = ({
         </svg>
       </button>
       <InputNumber
-        value={value}
+        value={value || localValue}
         {...rest}
         onChange={handleChange}
         classNameError='hidden'
